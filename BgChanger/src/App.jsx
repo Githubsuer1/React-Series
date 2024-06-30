35698ec755d37aa3ff831472d7ff8c16f9857a86
Siddhant Mishra <mishrasiddhant560@gmail.com>
2024-06-30 14:20:21 +0530

    update app.jsx

--- BgChanger/src/App.jsx
+++ BgChanger/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className='w-full h-screen absolute' style={{backgroundColor:color}}>
-      <h1 className='bg-blue-500 py-3 text-center text-3xl font-bold'>Calculator</h1>
+      <h1 className='bg-blue-500 py-3 text-center text-3xl font-bold'>BackGround Changer</h1>
         <div className='w-full h-32 absolute bottom-0 '>
           <div className='h-16 w-full bg-white flex items-center max-w-md rounded ml-auto mr-auto justify-center gap-3'>
             <button className='bg-red-600 text-white rounded-full py-2 px-3' onClick={()=>setColor('red')}>Red</button>
